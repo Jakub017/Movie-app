32243eec5e4ab5ec9da1f0f26056120945f8b245
Jakub Lipiński <j.lipinski017@gmail.com>
2023-04-29 17:13:52 +0200

    complete genres section, add popular movies section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,6 +30,9 @@
                     <a class="nav__menu-link" href="#">Genres</a>
                 </li>
                 <li class="nav__menu-item">
+                    <a class="nav__menu-link" href="#">Popular</a>
+                </li>
+                <li class="nav__menu-item">
                     <a class="nav__menu-link" href="#">About</a>
                 </li>
                 <li class="nav__menu-item">
@@ -173,11 +176,176 @@
                 </div>
             </div>
         </div>
+    </section>
 
+    <section class="section popular">
+        <div class="section__wrapper">
+            <div class="section__heading">
+                <h1 class="section__title">Most popular movies in 2023</h1>
+                <h3 class="section__subtitle">
+                    We present many films from various main categories, let's chosse and search film you like.
+                </h3>
+            </div>
+            <div class="popular__wrapper">
+                <div class="popular__genres">
+                    <ul class="popular__list">
+                        <li class="popular__item">
+                            <button class="popular__link popular__link--active">All</button>
+                        </li>
+                        <li class="popular__item">
+                            <button class="popular__link">Action</button>
+                        </li>
+                        <li class="popular__item">
+                            <button class="popular__link">Fantasy</button>
+                        </li>
+                        <li class="popular__item">
+                            <button class="popular__link">Comedy</button>
+                        </li>
+                        <li class="popular__item">
+                            <button class="popular__link">Drama</button>
+                        </li>
+                        <li class="popular__item">
+                            <button class="popular__link">Mystery</button>
+                        </li>
+                        <li class="popular__item">
+                            <button class="popular__link">Romance</button>
+                        </li>
+                        <li class="popular__item">
+                            <button class="popular__link">Horror</button>
+                        </li>
+                    </ul>
+                </div>
+                <div class="popular__movies">
+                    <div class="popular__movies-container">
+                        <div class="popular__movie">
+                            <img src="{{asset('img/movies-posters/ant-man-poster.jpg')}}" alt=""
+                                class="popular__poster">
+                            <h2 class="popular__title">Ant-Man and The Wasp: Quantumania</h2>
+                            <p class="popular__description">Ant-Man and the Wasp along with Hank Pym and Janet Van Dyne
+                                and
+                                Cassie Lang explore...</p>
+                        </div>
+                        <div class="popular__movie">
+                            <img src="{{asset('img/movies-posters/ant-man-poster.jpg')}}" alt=""
+                                class="popular__poster">
+                            <h2 class="popular__title">Ant-Man and The Wasp: Quantumania</h2>
+                            <p class="popular__description">Ant-Man and the Wasp along with Hank Pym and Janet Van Dyne
+                                and
+                                Cassie Lang explore...</p>
+                        </div>
+
+                        <div class="popular__movie">
+                            <img src="{{asset('img/movies-posters/ant-man-poster.jpg')}}" alt=""
+                                class="popular__poster">
+                            <h2 class="popular__title">Ant-Man and The Wasp: Quantumania</h2>
+                            <p class="popular__description">Ant-Man and the Wasp along with Hank Pym and Janet Van Dyne
+                                and
+                                Cassie Lang explore...</p>
+                        </div>
+                        <div class="popular__movie">
+                            <img src="{{asset('img/movies-posters/ant-man-poster.jpg')}}" alt=""
+                                class="popular__poster">
+                            <h2 class="popular__title">Ant-Man and The Wasp: Quantumania</h2>
+                            <p class="popular__description">Ant-Man and the Wasp along with Hank Pym and Janet Van Dyne
+                                and
+                                Cassie Lang explore...</p>
+                        </div>
+                        <div class="popular__movie">
+                            <img src="{{asset('img/movies-posters/ant-man-poster.jpg')}}" alt=""
+                                class="popular__poster">
+                            <h2 class="popular__title">Ant-Man and The Wasp: Quantumania</h2>
+                            <p class="popular__description">Ant-Man and the Wasp along with Hank Pym and Janet Van Dyne
+                                and
+                                Cassie Lang explore...</p>
+                        </div>
+                        <div class="popular__movie">
+                            <img src="{{asset('img/movies-posters/ant-man-poster.jpg')}}" alt=""
+                                class="popular__poster">
+                            <h2 class="popular__title">Ant-Man and The Wasp: Quantumania</h2>
+                            <p class="popular__description">Ant-Man and the Wasp along with Hank Pym and Janet Van Dyne
+                                and
+                                Cassie Lang explore...</p>
+                        </div>
+                        <div class="popular__movie">
+                            <img src="{{asset('img/movies-posters/ant-man-poster.jpg')}}" alt=""
+                                class="popular__poster">
+                            <h2 class="popular__title">Ant-Man and The Wasp: Quantumania</h2>
+                            <p class="popular__description">Ant-Man and the Wasp along with Hank Pym and Janet Van Dyne
+                                and
+                                Cassie Lang explore...</p>
+                        </div>
+                        <div class="popular__movie">
+                            <img src="{{asset('img/movies-posters/ant-man-poster.jpg')}}" alt=""
+                                class="popular__poster">
+                            <h2 class="popular__title">Ant-Man and The Wasp: Quantumania</h2>
+                            <p class="popular__description">Ant-Man and the Wasp along with Hank Pym and Janet Van Dyne
+                                and
+                                Cassie Lang explore...</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="popular__controls">
+                    <div class="popular__line"></div>
+                    <div class="popular__arrows">
+                        <div class="popular__arrow-wrapper popular__arrow-wrapper--next">
+                            <img class="popular__arrow" src="{{asset('img/arrow-left.png')}}" alt="">
+                        </div>
+                        <div class="popular__arrow-wrapper popular__arrow-wrapper--prev">
+                            <img class="popular__arrow" src="{{asset('img/arrow-right.png')}}" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <section class="section about">
+        <div class="section__wrapper">
+            <div class="section__heading">
+                <h1 class="section__title">About this app</h1>
+                <h3 class="section__subtitle">
+                    We present many films from various main categories, let's chosse and search film you like.
+                </h3>
+            </div>
+            <div class="aobut__wrapper">
+                <div class="about__item">
+                    <div class="about__icon-wrapper">
+                        <img src="{{asset('img/about/info-icon.png')}}" alt="" class="about__icon">
+                    </div>
+                    <div class="about__text">
+                        <h3 class="about__title">Lorem Ipsum</h3>
+                        <p class="about__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+                            vel fugit alias aspernatur accusantium neque eligendi nulla provident, ab natus?</p>
+                    </div>
+                </div>
+                <div class="about__item">
+                    <div class="about__icon-wrapper">
+                        <img src="{{asset('img/about/favourites-icon.png')}}" alt="" class="about__icon">
+                    </div>
+                    <div class="about__text">
+                        <h3 class="about__title">Lorem Ipsum</h3>
+                        <p class="about__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+                            vel fugit alias aspernatur accusantium neque eligendi nulla provident, ab natus?</p>
+                    </div>
+                </div>
+                <div class="about__item">
+                    <div class="about__icon-wrapper">
+                        <img src="{{asset('img/about/official-icon.png')}}" alt="" class="about__icon">
+                    </div>
+                    <div class="about__text">
+                        <h3 class="about__title">Lorem Ipsum</h3>
+                        <p class="about__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+                            vel fugit alias aspernatur accusantium neque eligendi nulla provident, ab natus?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
 </script>
+
 
 </html>
